1f28414b01aa94cd8ef30e67d4d43404830eb98e
Safalya7777777 <56562794+Safalya7777777@users.noreply.github.com>
2020-05-18 05:58:45 +0530

    Add files via upload
    no// codeforce 1353A

---
+++ NA.cpp
@@ -0,0 +1,26 @@
+//ononononono
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //no
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int m,n;
+        cin>>n>>m;
+        if(n==1)
+            cout<<0<<endl;
+        else if(n==2)
+            cout<<m<<endl;
+        else cout<<(m*2)<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
