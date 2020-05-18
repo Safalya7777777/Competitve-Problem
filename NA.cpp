df33ce050a4fd5b69fdcbeda4930b5ce97868128
Safalya7777777 <56562794+Safalya7777777@users.noreply.github.com>
2020-05-18 07:46:19 +0530

    Add files via upload
    ternary string 1354

--- NA.cpp
+++ NA.cpp
@@ -1,6 +1,28 @@
 //ononononono
 #include<bits/stdc++.h>
 using namespace std;
+int mn(int a,int b,int c)
+{
+    if(a<b&&a<c)
+        return a;
+    else if(b<a&&b<c)
+        return b;
+    else return c;
+}
+int mx(int a,int b,int c)
+{
+    if(a>b&&a>c)
+        return a;
+    else if(b>a&&b>c)
+    return b;
+    else return c;
+}
+int cal(int a,int b,int c)
+{
+    int minimum=mn(a,b,c);
+    int maximum=mx(a,b,c);
+    return (maximum-minimum);
+}
 int main()
 {
     //no
@@ -8,13 +30,25 @@ int main()
     cin>>t;
     while(t--)
     {
-        long long int m,n;
-        cin>>n>>m;
-        if(n==1)
-            cout<<0<<endl;
-        else if(n==2)
-            cout<<m<<endl;
-        else cout<<(m*2)<<endl;
+        string s;
+        cin>>s;
+        int a[4];
+        int l=200001,flag=0;
+        a[1]=0,a[2]=0,a[3]=0;
+        for(int i=0;i<s.length();i++)
+        {
+            a[s[i]-48]=i+1;
+            if(a[1]!=0&&a[2]!=0&&a[3]!=0)
+            {
+                flag=1;
+                int ml=cal(a[1],a[2],a[3]);
+                if(ml<l)
+                    l=ml;
+            }
+        }
+        if(flag==1)
+        cout<<l+1<<endl;
+        else cout<<0<<endl;
     }
     return 0;
 }
